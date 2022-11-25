0a6156fb0ef1f7bd84d56c3fc0671a777cf4df22
Albert Chen <ajchen7712@gmail.com>
2022-11-25 16:15:15 -0500

    ignored

--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,8 @@
 
 #generate fake artists with name, year founded, country
 
-# generate fake songs with song name, genre, artist
+#generate fake songs with song name, genre, artist, and 5 top genres
 
 #generate fake users with username, account created, password
+
+#generate fake plays 
